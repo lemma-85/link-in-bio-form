982dddfcb3d6c442d198428fc11bfed289a0219d
lemma-85 <rmamuye@chicagobooth.edu>
2025-05-03 13:44:19 +0000

    added a backdoor page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,23 @@
 class ItemsController < ApplicationController
+
   def index
     @list_of_items = Item.all
 
     render({ :template => "item_templates/list" })
   end
+
+  def display_form
+
+    render ({:template => "item_templates/backdoor_form"})
+  end 
+
+  def create_item
+    i = Item.new
+    i.link_url = params.fetch("link_url_param")
+    i.link_description = params.fetch("link_description_param")
+    i.thumbnail_url = params.fetch("thumbnail_url_param")
+    i.save
+    redirect_to("/")
+  end
+
 end
